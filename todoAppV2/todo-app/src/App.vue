f5f118abd3644e4b7a7f1f9e7568c63d6177dcd1
gusholguin <augustin.holguin@gmail.com>
2018-01-11 14:07:18 +0100

    addTodo working

--- todoAppV2/todo-app/src/App.vue
+++ todoAppV2/todo-app/src/App.vue
@@ -18,5 +18,6 @@ export default {
  * {
    margin: 0;
    padding: 0;
+   font-family: arial;
  }
 </style>
--- todoAppV2/todo-app/src/components/Todo.vue
+++ todoAppV2/todo-app/src/components/Todo.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="todoapp">
     <h1>Todo App</h1><br>
-    <input type="text" v-model="newTodo" @keyup.enter="addTodo"></input>
+    <input type="text" placeholder="Add a todo" v-model="newTodo" @keyup.enter="addTodo"></input>
     <button @click="addTodo">Add</button>
-    <ul>
-      <li v-for="i in todo">{{ i }}</li>
-    </ul>
+    <div class="todolist">
+      <ul>
+        <li v-for="i in todo">
+          {{ i }}
+          <button class="destroy" @click="destroyTodo">x</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -13,13 +18,28 @@
 export default {
   data () {
     return {
-      todo: ['hello', 'aujus'],
-      newTodo: ''
+      todo: [],
+      newTodo: '',
+      index: 0
     }
+  },
+  mounted () {
+    this.todo.forEach((newTodo) => {
+      this.newTodo.index = i
+    })
   },
   methods: {
     addTodo () {
-      console.log('Workin')
+      if (this.newTodo === '') {
+        alert('Please enter your todo')
+      } else {
+      this.todo.push(this.newTodo)
+      this.newTodo = ''
+      console.log(this.todo.length)
+      }
+    },
+    destroyTodo () {
+      this.todo.splice(this.newTodo)
     }
   }
 }
@@ -30,6 +50,10 @@ export default {
   .todoapp {
     text-align: center;
     margin-top: 30px;
+    position: absolute;
+    left: 50%;
+    margin-left: -156px;
+    box-sizing: border-box;
   }
 
   h1 {
@@ -45,7 +69,42 @@ export default {
     line-height: 20px;
     width: 40px;
     border-radius: 5px;
-    margin-left: 5px;
+    opacity: 0.8;
+  }
+
+  button:hover {
+    cursor: pointer;
+    opacity: 1;
+  }
+
+  .todolist {
+    margin-top: 20px;
+  }
+
+  .todolist li {
+    box-sizing: border-box;
+    list-style-type: none;
+    border: solid 2px #49c0b6;
+    border-radius: 5px;
+    line-height: 30px;
+    margin: 10px;
+    width: 300px;
+  }
+
+  button.destroy {
+    position: absolute;
+    right: 12px;
+    border: none;
+    border-radius: 2px;
+    height: 30px;
+    width: 30px;
+    opacity: 0.8;
+    color: #6d6e70;
+  }
+
+  button.destroy:hover {
+    opacity: 1;
+    cursor: pointer;
   }
 
 </style>
